fd58cf51208d6b0cfbc8a2541cbd2419dac33f00
Aleksander Seryogin <a.siarohin@gmail.com>
2018-11-05 00:28:59 +0300

    Init commit

---
+++ js/youtube-module.js
@@ -0,0 +1,19 @@
+// document.body.innerHTML = `<div id="wrapper">
+// <header>
+//   <div class="search-bar">
+//     <input id="search" type="text" name="search" value="" autocomplete="off" placeholder="Search">
+//   </div>
+// </header>
+
+// </div>`;
+
+// let onHeader = `<div id="wrapper">
+// <header>
+//   <div class="search-bar">
+//     <input id="search" type="text" name="search" value="" autocomplete="off" placeholder="Search">
+//   </div>
+// </header>
+
+// </div>`;
+
+// document.body.insertAdjacentHTML('beforeend', onHeader);
