72675a387fc99f55e391449d4bef3c21c0f60ada
Aleksander Seryogin <a.siarohin@gmail.com>
2018-11-20 01:35:54 +0300

    feat: - create navigations, search, sliders, transform in modules

---
+++ src/js/settings.js
@@ -0,0 +1,14 @@
+// set max results of blocks
+export const MAX_RESULT = 50;
+
+// width of slider
+export const GLOBAL_BLOCK_SETTING = 340;
+
+// video id
+export const youtube = [];
+
+// api key
+export const apiKey = 'AIzaSyCAznfTwZKs8R47J-_PkpBrHYaRvcCmKwY';
+
+// url
+export const url = 'https://www.googleapis.com/youtube/v3';
